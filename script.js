bbf0f3190c4afae47b5dc11ca9e65b2742d7aff7
Pedro <pedroliveiralima21@gmail.com>
2025-06-13 19:06:17 -0300

    feat(cardTrick): Add colors in dificult and level

--- script.js
+++ script.js
@@ -4,8 +4,34 @@ document.addEventListener("DOMContentLoaded", () => {
   const localTrick = document.getElementById("localTrick");
   const categoria = document.getElementById("categoria");
   const dificuldade = document.getElementById("dificuldade");
-  const status = document.getElementById("status");
   const listTricks = document.getElementById("listTricks");
+
+  const updateColorSelect = (selectElement) => {
+    if (selectElement.value === "Aprendido") {
+      selectElement.style.background = "green";
+      selectElement.style.color = "white";
+    } else if (selectElement.value === "Em processo") {
+      selectElement.style.background = "blue";
+      selectElement.style.color = "white";
+    } else {
+      selectElement.style.background = "red";
+      selectElement.style.color = "white";
+    }
+  };
+
+  const updateLevel = (level) => {
+    const text = level.textContent.toLowerCase();
+    if (text === "fácil") {
+      level.style.background = "green";
+      level.style.color = "white";
+    } else if (text === "média") {
+      level.style.background = "yellow";
+      level.style.color = "black";
+    } else {
+      level.style.background = "red";
+      level.style.color = "white";
+    }
+  };
 
   form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -13,24 +39,35 @@ document.addEventListener("DOMContentLoaded", () => {
       nameTrick.value.trim() &&
       localTrick.value.trim() &&
       categoria.value &&
-      dificuldade.value &&
-      status.value
+      dificuldade.value
     ) {
       const cardTrick = document.createElement("div");
       cardTrick.classList.add("trick-card");
 
       cardTrick.innerHTML = `
           <h3>${nameTrick.value.trim()}</h3>
-           <p><strong>Local:</strong>${localTrick.value.trim()}</p>
-           <p><strong>Categoria:</strong>${categoria.value}</p>
+           <p><strong>Local:</strong> ${localTrick.value.trim()} </p>
+           <p><strong>Categoria:</strong> ${categoria.value} </p>
           <div class="cardStatus">
             <p class="dificult">${dificuldade.value}</p>
-            <p class="status">${status.value}</p>
+              <select id="status-select">
+                <option value="Aprendido">Aprendido</option>
+                <option value="Em processo">Em processo</option>
+                <option value="Não aprendida">Não aprendida</option>
+              </select>
           </div>
         `;
 
-      listTricks.appendChild(cardTrick);
+      const statusSelect = cardTrick.querySelector("#status-select");
+      updateColorSelect(statusSelect);
+      statusSelect.addEventListener("change", () =>
+        updateColorSelect(statusSelect)
+      );
 
+      const levelSelect = cardTrick.querySelector(".dificult");
+      updateLevel(levelSelect);
+
+      listTricks.appendChild(cardTrick);
       alert("Trick nova adicionada com sucesso !");
       form.reset();
     } else {
